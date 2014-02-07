5cf249a98526ceede4ac4728470e04295e282205
Tomas van Rijsse <tomas@pionect.nl>
2014-02-07 19:45:18 +0100

    simplify image item styling

--- assets/item.instagram.php
+++ assets/item.instagram.php
@@ -1,10 +1,10 @@
 <?php /* @var $item pnct_socialstream_item */ 
 $content = unserialize($item->content); ?>
-<div class="ss_item <?php echo $item->type; ?>" data-url="<?php echo $item->url; ?>">
-    <div class="ss_content" title="<?php echo $item->content; ?>" style="background-image:url(<?php echo $item->thumbnail; ?>)">
+<div class="ss_item image <?php echo $item->type; ?>" data-url="<?php echo $item->url; ?>">
+    <div class="ss_content" title="<?php echo $item->content; ?>" style="background-image:url(<?php echo $content['image']; ?>);">
     </div>
     <div class="ss_username">
-        <?php echo $item->post_name; ?>
+        <?php echo $content['caption'];//$item->post_name; ?>
     </div>
     <div class="ss_typeicon"></div>
 </div>
--- assets/item.vimeo.php
+++ assets/item.vimeo.php
@@ -1,5 +1,5 @@
 <?php /* @var $item pnct_socialstream_item */ ?>
-<div class="ss_item <?php echo $item->type; ?>" data-url="<?php echo $item->url; ?>">
+<div class="ss_item image <?php echo $item->type; ?>" data-url="<?php echo $item->url; ?>">
     <div class="ss_content" title="<?php echo $item->content; ?>" style="background-image:url(<?php echo $item->thumbnail; ?>)">
     </div>
     <div class="ss_username">
