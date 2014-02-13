2c0d1622a50511d58c49aeb069475a856f0ed207
Tomas van Rijsse <tomas@pionect.nl>
2014-02-13 15:19:06 +0100

    Added notices

--- includes/settings.php
+++ includes/settings.php
@@ -4,6 +4,8 @@ class pnct_socialstream_settings {
 	function __construct() {
         //voor het knopje in het menu
 		add_action('admin_menu', array(&$this, 'admin_menu'));
+        //voor de notices
+        add_action( 'admin_notices', array(&$this, 'notices_action') );
         //voor de knop 'manual import' op de settings page
         add_action('admin_post_pnct_socialstream_startcron', array(&$this, 'startImportManual'));
         add_action('admin_post_pnct_socialstream_truncate', array(&$this, 'truncate'));
@@ -17,6 +19,11 @@ class pnct_socialstream_settings {
         add_action('admin_init', array(&$this,'init_settings'));
 	}
     
+    function notices_action() {
+        settings_errors( 'pnct_socialstream' );
+    }
+    
+    
     function init_settings(){
         register_setting('socialstream', 'socialstream_color',array(&$this,'checkColor'));
         register_setting('socialstream', 'socialstream_useraccounts',array(&$this,'checkAccounts'));
@@ -29,7 +36,7 @@ class pnct_socialstream_settings {
     
     function checkAccounts($input){
         $oldaccounts = get_option('socialstream_useraccounts');
-        
+        $errors = array();
         //in future loop over $input
         //find facebook id, flickr_id 
         if($input['instagram_username']!=""){
@@ -45,16 +52,41 @@ class pnct_socialstream_settings {
 
                 $context  = stream_context_create($options);
                 $result = json_decode(file_get_contents($url, false, $context));
+                $usernamecheck = false;
                 if($result){  
-                    if($result->meta->code==200 && count($result->data)>0){
-                        $input['instagram_id'] = $result->data[0]->id;
+                    if($result->meta->code==200){
+                        if(count($result->data)>0){
+                            $input['instagram_id'] = $result->data[0]->id;
+                            $usernamecheck = true;
+                        } else {
+                            $errors[] = 'No exact match found for instagram';
+                        }
+                    } else {
+                        $errors[] = 'No account found for instagram';
                     }
+                }
+                if(!$usernamecheck){
+                    $input['instagram_id'] = '';
+                    $input['instagram_username'] = '';
                 }
             } else {
                 $input['instagram_id'] = $oldaccounts['instagram_id'];
             }
+            
+            
+            //fetch the Facebook ID
+            
+            //fetch the Flickr ID
         }
         
+        if(count($errors)>0){
+            add_settings_error(
+                'pnct_socialstream',
+                esc_attr( 'failed_accounts' ),
+                print_r($input,true),
+                'error'
+            );
+        }
         return $input;
     }
     
